c55c97a1dcae87e44d3610c0d40103fdb9192221
Izaz Dhiyaulhaq <59567289+izazdhiya@users.noreply.github.com>
2021-11-04 09:05:50 +0700

    Update jimpitan.dart

--- lib/view/jimpitan.dart
+++ lib/view/jimpitan.dart
@@ -1,6 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:jimpitan/view/tambahjimpitan.dart';
+import 'package:intl/intl.dart';
 
 class Jimpitan extends StatefulWidget {
   const Jimpitan({Key? key}) : super(key: key);
@@ -10,13 +11,14 @@ class Jimpitan extends StatefulWidget {
 }
 
 class _JimpitanState extends State<Jimpitan> {
+  TextEditingController _nama = TextEditingController();
+  TextEditingController _jumlah = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // Size screenSize = MediaQuery.of(context).size;
-    // int heightSize = screenSize.height.toInt();
-    // int widthSize = screenSize.width.toInt();
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference jimpitan = firestore.collection('jimpitan');
     return Scaffold(
       appBar: AppBar(
           title: Text("Jimpitan Hari Ini",
@@ -24,11 +26,26 @@ class _JimpitanState extends State<Jimpitan> {
                   textStyle:
                       TextStyle(fontSize: 14, fontWeight: FontWeight.w500))),
           backgroundColor: Color(0xFFFF5521)),
-      body: LayoutBuilder(
-          builder: (context, index) => Container(
-                  child: Center(
-                child: Text("Tidak Ada Data"),
-              ))),
+      body: ListView(children: [
+        StreamBuilder<QuerySnapshot>(
+          stream: jimpitan
+              .where('tanggal',
+                  isGreaterThan: DateTime.now().subtract(Duration(days: 1)))
+              .orderBy('tanggal', descending: true)
+              .snapshots(),
+          builder: (_, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: snapshot.data!.docs
+                    .map((e) => ItemCard(e['nama'], e['jumlah'], e))
+                    .toList(),
+              );
+            } else {
+              return Center(child: Center(child: Text('Loading')));
+            }
+          },
+        )
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
@@ -50,6 +67,7 @@ class _JimpitanState extends State<Jimpitan> {
                               }
                               return null;
                             },
+                            controller: _nama,
                             decoration: InputDecoration(
                               labelText: 'Nama Warga',
                               icon: Icon(Icons.person),
@@ -62,6 +80,7 @@ class _JimpitanState extends State<Jimpitan> {
                               }
                               return null;
                             },
+                            controller: _jumlah,
                             decoration: InputDecoration(
                               labelText: 'Jumlah',
                               icon: Icon(Icons.attach_money),
@@ -81,10 +100,13 @@ class _JimpitanState extends State<Jimpitan> {
                         color: Color(0xFF0043A7),
                         onPressed: () {
                           if (_formKey.currentState!.validate()) {
-                            // warga.add({
-                            //   'nama': _nama.text,
-                            // });
-                            // _nama.text = '';
+                            jimpitan.add({
+                              'tanggal': DateTime.now(),
+                              'nama': _nama.text,
+                              'jumlah': int.tryParse(_jumlah.text)
+                            });
+                            _nama.text = '';
+                            _jumlah.text = '';
                             ScaffoldMessenger.of(context).showSnackBar(
                               const SnackBar(
                                   content: Text('Data berhasil disimpan!')),
@@ -96,8 +118,6 @@ class _JimpitanState extends State<Jimpitan> {
                   ],
                 );
               });
-          // Navigator.push(context,
-          //     MaterialPageRoute(builder: (context) => TambahJimpitan()));
         },
         tooltip: 'Tambah',
         child: Icon(Icons.add),
@@ -121,17 +141,233 @@ class _JimpitanState extends State<Jimpitan> {
                           fontWeight: FontWeight.w500))),
             ),
             Container(
-              child: Text(
-                  "Rp 200.000", //+ setTotal(data, widget.index, widget.counter),
-                  style: GoogleFonts.poppins(
-                      textStyle: TextStyle(
-                          color: Color(0xFFFAFAFA),
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500))),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: jimpitan
+                    .where('tanggal',
+                        isGreaterThan:
+                            DateTime.now().subtract(Duration(days: 1)))
+                    .snapshots(),
+                builder: (_, snapshot) {
+                  if (snapshot.hasData) {
+                    List myDocCount =
+                        snapshot.data!.docs.map((e) => e['jumlah']).toList();
+                    num jumlahjimpitan = 0;
+                    for (var i in myDocCount) {
+                      jumlahjimpitan = jumlahjimpitan + i;
+                    }
+                    return Text(
+                        NumberFormat.simpleCurrency(locale: 'id')
+                            .format(jumlahjimpitan),
+                        style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                                color: Color(0xFFFAFAFA),
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500)));
+                  } else {
+                    return Center(
+                        child: Center(
+                            child: Text('Rp 0',
+                                style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                        color: Color(0xFFFAFAFA),
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500)))));
+                  }
+                },
+              ),
             )
           ],
         ),
       )),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final String nama;
+  final int jumlah;
+  final e;
+
+  ItemCard(this.nama, this.jumlah, this.e);
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    TextEditingController _nama = TextEditingController();
+    TextEditingController _jumlah = TextEditingController();
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference jimpitan = firestore.collection('jimpitan');
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(left: 15, top: 5, right: 15, bottom: 5),
+      padding: EdgeInsets.only(left: 10, top: 5, right: 10, bottom: 5),
+      decoration: BoxDecoration(
+        color: Color(0xFFFAFAFA),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            spreadRadius: 3,
+            blurRadius: 3,
+          )
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(nama,
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w600, fontSize: 16)),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(
+                    NumberFormat.simpleCurrency(locale: 'id').format(jumlah),
+                    style: GoogleFonts.poppins(
+                        color: Color(0xFF348A36),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 12)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                height: 40,
+                width: 60,
+                child: MaterialButton(
+                    shape: CircleBorder(),
+                    color: Colors.yellow[600],
+                    child: Center(
+                        child: Icon(
+                      Icons.edit,
+                      color: Colors.white,
+                    )),
+                    onPressed: () {
+                      _nama.text = nama;
+                      _jumlah.text = jumlah.toString();
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              scrollable: true,
+                              title: Text('Edit Jimpitan'),
+                              content: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Form(
+                                  key: _formKey,
+                                  child: Column(
+                                    children: <Widget>[
+                                      TextFormField(
+                                        validator: (value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Nama tidak boleh kosong';
+                                          }
+                                          return null;
+                                        },
+                                        controller: _nama,
+                                        decoration: InputDecoration(
+                                          labelText: 'Nama Warga',
+                                          icon: Icon(Icons.person),
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        validator: (value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Jumlah tidak boleh kosong';
+                                          }
+                                          return null;
+                                        },
+                                        controller: _jumlah,
+                                        decoration: InputDecoration(
+                                          labelText: 'Jumlah',
+                                          icon: Icon(Icons.attach_money),
+                                        ),
+                                        keyboardType: TextInputType.number,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              actions: [
+                                MaterialButton(
+                                    child: Text(
+                                      "Simpan",
+                                      style:
+                                          TextStyle(color: Color(0xFFFAFAFA)),
+                                    ),
+                                    color: Color(0xFF117B00),
+                                    onPressed: () {
+                                      if (_formKey.currentState!.validate()) {
+                                        jimpitan.doc(e.id).update({
+                                          'nama': _nama.text,
+                                          'jumlah': int.tryParse(_jumlah.text)
+                                        });
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                          const SnackBar(
+                                              content: Text(
+                                                  'Data berhasil disimpan!')),
+                                        );
+
+                                        Navigator.of(context).pop();
+                                      }
+                                    })
+                              ],
+                            );
+                          });
+                    }),
+              ),
+              SizedBox(
+                height: 40,
+                width: 60,
+                child: MaterialButton(
+                    shape: CircleBorder(),
+                    color: Colors.red[900],
+                    child: Center(
+                        child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    )),
+                    onPressed: () {
+                      AlertDialog alert = AlertDialog(
+                        title: Text("Konfirmasi"),
+                        content: Text("Yakin ingin menghapus data?"),
+                        actions: [
+                          MaterialButton(
+                            color: Color(0xFFDF0000),
+                            child: Text("YA",
+                                style: TextStyle(
+                                  color: Color(0xFFFAFAFA),
+                                )),
+                            onPressed: () {
+                              jimpitan.doc(e.id).delete();
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Data berhasil dihapus!')),
+                              );
+                              Navigator.of(context).pop();
+                            },
+                          )
+                        ],
+                      );
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return alert;
+                        },
+                      );
+                    }),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
